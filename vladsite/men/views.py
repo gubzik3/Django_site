77f1f885cf99928e436f9ae4ce0cc44f13b74e69
gubzik3 <vladlapin03@yandex.ru>
2025-04-08 21:10:27 +0500

    day 2

--- vladsite/men/views.py
+++ vladsite/men/views.py
@@ -1,6 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Страница приложения men")
+
+def name_men(request):
+    return HttpResponse("<h1>Мужчины по именам<h1>")
+
+def proverka(request, menid):
+    if(request.POST):
+        print(request.POST)
+
+    return HttpResponse(f"<h1>Проверочный запрос<h1><p>{menid}<p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
